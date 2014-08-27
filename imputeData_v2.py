c14d3f793a3bfa4dd00361319a529923c02b9acd
Goutham <goutham.atla@gmail.com>
2014-08-27 17:53:11 +0530

    get number of samples from file itself

--- imputeData_v2.py
+++ imputeData_v2.py
@@ -1,15 +1,10 @@
-# This program takes structure file as input
-# At each locus in structure file, keeps 0 randomly for user defines number of samples i.e creating missing data randomly
-# This program reads entire file in to memory, hence may perform slow, but will do the job.
-# The performance will be improved in future.
-# Usage: python program.py > Output.structure
- 
+#This program 
 from __future__ import print_function
 import random
 
-num_samples=48
+
 missing_samples=24
-file_name="file2"
+file_name="file4"
 
 sample_names=[]
 population_info=[]
@@ -17,15 +12,19 @@ locus_names=[]
 main_array=[]
 
 temp_array=[]
+
+num_samples=0
 for line in open(file_name,'r'):
     if line.startswith("\t"):
         locus_names.append(line)
-
+        
     if not line.startswith("\t"):
         splitted=line.split("\t")
         data_columns = (len(splitted[2].strip().split()))
         sample_names.append(splitted[0])
         population_info.append(splitted[1])
+        num_samples+=1
+
 
 for i in range(0,data_columns):
     for line in open(file_name,'r'):
@@ -33,7 +32,7 @@ for i in range(0,data_columns):
             splitFile=line.split("\t")
             snp_data=splitFile[2].strip().split()     
             temp_array.append(snp_data[i])
-    for x in random.sample(range(1,num_samples*2,2),missing_samples):
+    for x in random.sample(range(1,num_samples,2),missing_samples):
         temp_array[x]=0
         temp_array[x-1]=0
     main_array.append(temp_array)
@@ -45,10 +44,13 @@ main_array.insert(1,population_info)
 for locus in locus_names:
     print (locus)
 
-x_MAX=num_samples*2
+x_MAX=num_samples
 y_MAX=data_columns+2
 
 for x in range(0,x_MAX):
     for y in range(0,y_MAX):
-        print (main_array[y][x],end="\t")
-    print ("\n")
+        if y<=1:
+            print (main_array[y][x],end="\t")
+        else:
+            print (main_array[y][x],end=" ")
+    print ()
