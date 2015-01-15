a8758a8a642b19e72b2549cf56bc43a7098c44f9
Goutham <goutham.atla@gmail.com>
2015-01-15 12:50:17 +0530

    Update realign_bam_parallel.sh

--- realign_bam_parallel.sh
+++ realign_bam_parallel.sh
@@ -1,18 +1,23 @@
 # This script should be placed in a folder where the bam files resides and then run `sh realign_bam_parallel.sh`
 #This script 1) adds read group information 2)index the bam files 3) create interval files 4) realign the bam files
 
+$ncores=4 #Change this parameter depending on number of cores in your computer
+
 #Add readgroup information
-parallel java -Xmx8g -jar picard-tools-1.106/AddOrReplaceReadGroups.jar I={} O=`echo {.} | awk '{ print \$1"_RG.bam"}'` RGID=`echo {.} | awk '{ print \$1 }'` RGLB=lib1 RGPL=illumina RGPU=unit1 RGSM=`echo {.} | awk '{ print \$1 }'` ::: *.bam
+parallel --jobs $ncores java -Xmx8g -jar picard-tools-1.106/AddOrReplaceReadGroups.jar I={} O={.}_RG.bam RGID={.} RGLB=lib1 RGPL=illumina RGPU=unit1 RGSM={.} ::: *.bam
 
 #index bam
-parallel samtools index {} ::: sorted*_RG.bam
+parallel --jobs $ncores samtools index {} ::: sorted*_RG.bam
 
 #Create Interval File
-parallel java -Xmx8g -jar GenomeAnalysisTK.jar -T RealignerTargetCreator -R Genome.fa -I {} -o `echo {.} | awk '{ print $1".intervals"}'` ::: sorted*_RG.bam
+parallel --jobs $ncores java -Xmx8g -jar GenomeAnalysisTK.jar -T RealignerTargetCreator -R Genome.fa -I {} -o {.}.intervals ::: sorted*_RG.bam
 wait 
 
 #realign bases around indels
-parallel  java -Xmx8g -jar GenomeAnalysisTK.jar -T IndelRealigner -R Genome.fa -I {} -targetIntervals `echo {.} | awk '{ print $1".intervals"}'`  -o `echo {.} | awk '{ print $1"_realigned.bam"}'` ::: sorted*_RG.bam
+parallel --jobs $ncores java -Xmx8g -jar GenomeAnalysisTK.jar -T IndelRealigner -R Genome.fa -I {} -targetIntervals {.}.intervals  -o {.}_realigned.bam ::: sorted*_RG.bam
 
 #index realigned files
-parallel samtools index {} ::: sorted*_realigned.bam
+parallel --jobs $ncores samtools index {} ::: sorted*_realigned.bam
+
+#call SNP using samtools 
+parallel --jobs $ncores "samtools mpileup -E -uf Genome.fa {} | bcftools view -cg - | vcfutils.pl varFilter -d 3 > {.}.vcf" ::: sorted*_realigned.bam
