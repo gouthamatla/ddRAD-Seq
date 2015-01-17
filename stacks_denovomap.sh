c65b34ff37b04c7259fcf727401a1c46e957b377
Goutham <goutham.atla@gmail.com>
2015-01-17 17:01:01 +0530

    Update stacks_denovomap.sh

--- stacks_denovomap.sh
+++ stacks_denovomap.sh
@@ -1,3 +1,16 @@
+#the process_radtags program takes a barcode file i.e 5bp internal barcode and 6bp illumina adapter sequence in a tab delimited file format.
+#See the example file 'barcodes.txt'
+
+#keep all the sample names in a text file. see the 'sample_names.txt'
+
+#To rename the process_radtags output file to original sample names, run the following four commands.
+while read line; do cp `echo $line | awk '{ print $2".1.fq.gz " $1".1.fq.gz"}'` ; done < sample_names.txt
+while read line; do cp `echo $line | awk '{ print $2".2.fq.gz " $1".2.fq.gz"}'` ; done < sample_names.txt
+while read line; do cp `echo $line | awk '{ print $2".rem.1.fq.gz " $1".rem.1.fq.gz"}'` ; done < sample_names.txt
+while read line; do cp `echo $line | awk '{ print $2".rem.2.fq.gz " $1".rem.2.fq.gz"}'` ; done < sample_names.txt
+
+
+
 #get the list of samples into an a string using the followin python one liner.
 
 #Assuming that the Sample name ends with .fq
