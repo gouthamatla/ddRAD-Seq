fd83df9b3e4674906992887f0f26a381af48f05b
Goutham <goutham.atla@gmail.com>
2015-01-17 16:55:46 +0530

    Update stacks_denovomap.sh

--- stacks_denovomap.sh
+++ stacks_denovomap.sh
@@ -1,7 +1,7 @@
 #get the list of samples into an a string using the followin python one liner.
 
 #Assuming that the Sample name ends with .fq
-samples=`python -c 'import os; print "-s "+" -s ".join([x for x in os.listdir(".") if x.endswith(".fq")])'`
+samples=`python -c 'import os; print " -s "+" -s ".join([x for x in os.listdir(".") if x.endswith(".fq")])'`
 
 ncores=4
 
