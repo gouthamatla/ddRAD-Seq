17b2ea59d43580c8dbd5a3c61d020f6a25453285
Goutham <goutham.atla@gmail.com>
2015-01-17 16:54:39 +0530

    Create stacks_denovomap.sh

---
+++ stacks_denovomap.sh
@@ -0,0 +1,10 @@
+#get the list of samples into an a string using the followin python one liner.
+
+#Assuming that the Sample name ends with .fq
+samples=`python -c 'import os; print "-s "+" -s ".join([x for x in os.listdir(".") if x.endswith(".fq")])'`
+
+ncores=4
+
+#Run stacks denovo_map.pl
+
+denovo_map.pl $samples -b 1 -o ouputFolder -S -T $ncores
