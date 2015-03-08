95f4621f2fedd680959faad8f849456abf6817e9
Goutham <goutham.atla@gmail.com>
2015-03-08 20:29:11 +0530

    Update realign_bam_parallel.sh

--- realign_bam_parallel.sh
+++ realign_bam_parallel.sh
@@ -1,7 +1,7 @@
 # This script should be placed in a folder where the bam files resides and then run `sh realign_bam_parallel.sh`
 #This script 1) adds read group information 2)index the bam files 3) create interval files 4) realign the bam files
 
-$ncores=4 #Change this parameter depending on number of cores in your computer
+ncores=4 #Change this parameter depending on number of cores in your computer
 
 #Add readgroup information
 parallel --jobs $ncores java -Xmx8g -jar picard-tools-1.106/AddOrReplaceReadGroups.jar I={} O={.}_RG.bam RGID={.} RGLB=lib1 RGPL=illumina RGPU=unit1 RGSM={.} ::: *.bam
